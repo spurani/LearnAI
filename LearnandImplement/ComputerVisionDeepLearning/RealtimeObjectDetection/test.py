2574c3255a802b8188083ccc8e35cffae172f8b3
Shlok <shlokpurani@hotmail.com>
2022-03-20 12:51:37 -0400

    This is extended update for test file

--- LearnandImplement/ComputerVisionDeepLearning/RealtimeObjectDetection/test.py
+++ LearnandImplement/ComputerVisionDeepLearning/RealtimeObjectDetection/test.py
@@ -11,3 +11,17 @@ ap=argparse.ArgumentParser()
 ap.add_argument("-i",required=True,help="path to image",dest="bar")
 args=vars(ap.parse_args())
 print(args["bar"])
+image=cv2.imread(args["bar"])
+image=cv2.resize(image,(500,500))
+cv2.imshow("frame",image)
+cv2.waitKey(0)
+cv2.destroyWindow("frame")
+while True:
+    image1=cv2.imread(args["bar"])
+    image1=cv2.resize(image1,(500,500))
+    cv2.imshow("frame1",image1)
+    k = cv2.waitKey(1)
+    if(k == ord('q')):
+        break
+cv2.destroyWindow("frame1")
+cv2.destroyAllWindows()
